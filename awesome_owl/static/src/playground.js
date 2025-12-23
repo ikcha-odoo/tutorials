9a1755027297942ddec4e114383749e0985e8d31
Jack41784090 <tszmingjack@gmail.com>
2025-12-23 15:32:54 +0800

    Update playground to integrate Card and Counter components

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,11 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Counter } from "./Counter/counter";
+import { Card } from "./Card/card";
+
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card }
+
+    markup = markup("<div> Test </div>")
 }
