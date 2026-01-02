bbe478c858916593cf39df56c167aaae9c2533a6
ikcha.odoo <ikcha@odoo.com>
2026-01-02 10:02:57 +0800

    Estate module tutorial 5-8

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,9 +8,10 @@
     'depends': ['base'],
     'data': [
         # Add your data files here (views, security, etc)
-        # 'security/ir.model.access.csv',
-        # 'views/your_views.xml',
+        'security/ir.model.access.csv',
+        'views/estate.view-properties.xml',
     ],
     'installable': True,
+    'application': True,
     'auto_install': False,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,29 @@
 from odoo import fields, models
+from datetime import date, datetime, timedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = "A property that you can probably not afford."
     # _order = "sequence"
+    
+    active = fields.Boolean('Active', default=True)
+    state=fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer received', 'Offer Received'),
+            ('accepted', 'Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
+        ]
+    )
 
     name = fields.Char('Name', required=True, translate=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Available From')
-    expected_price = fields.Float('Expected Price')
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
+    date_availability = fields.Date('Available From', default=date.today()+timedelta(days=90))
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', readonly=True)
+    bedrooms = fields.Integer('Bedrooms', default=2)
     living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
