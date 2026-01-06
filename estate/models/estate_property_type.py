f0ce1f7c45acdb8bfa60022f7430aea1633e08b3
Jack41784090 <tszmingjack@gmail.com>
2026-01-06 09:35:45 +0800

    Enhance estate_property_type model

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,7 +3,18 @@ from odoo import models, fields
 class EstateType(models.Model):
     _name = "estate.property.type"
     _description = "The type of estate"
+    _order = "sequence"
 
+    color = fields.Integer('Color')
+    sequence = fields.Integer('Sequence', default=0)
     name = fields.Char("Name", required=True)
+    # properties_ids = fields.One2many("estate.property", "estate_type")
+
+    property_ids = fields.One2many("estate.property", "tags_ids", index=True)
+
+    _check_uniqueness_name = models.Constraint(
+        'unique(name)',
+        "Name must be unique."
+    )
 
 
