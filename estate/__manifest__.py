8e88008d5beeb30c208fb4bf0e1ab2b52977bbed
Jack41784090 <tszmingjack@gmail.com>
2026-01-06 17:04:15 +0800

    Update estate views: add kanban view, modify property views to include tags, update menu and actions

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,8 +10,10 @@
         # Add your data files here (views, security, etc)
         'security/ir.model.access.csv',
         'views/estate.view-properties.xml',
+        'views/res_users_views.xml',
+        'views/kanban.xml'
     ],
     'installable': True,
     'application': True,
     'auto_install': False,
-}
+} # type: ignore 
