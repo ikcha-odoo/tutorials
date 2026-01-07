732621ce3d224fccb7b31dfcaa19992ac9ff4771
Jack41784090 <tszmingjack@gmail.com>
2026-01-07 10:28:27 +0800

    chapter 15 finished with refactoring ending it

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,11 +7,17 @@
     'author': 'Your Name',
     'depends': ['base'],
     'data': [
-        # Add your data files here (views, security, etc)
         'security/ir.model.access.csv',
-        'views/estate.view-properties.xml',
+        'views/estate_menuitems.xml',
+        'views/estate_type_action.xml',
+        'views/estate_type_views.xml',
+        'views/estate_tag_action.xml',
+        'views/estate_tag_views.xml',
+        'views/estate_property_action.xml',
+        'views/estate_property_views.xml',
+        'views/estate_offer_action.xml',
+        'views/estate_offer_views.xml',
         'views/res_users_views.xml',
-        'views/kanban.xml'
     ],
     'installable': True,
     'application': True,
